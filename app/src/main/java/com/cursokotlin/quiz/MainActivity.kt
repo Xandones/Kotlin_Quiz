6eda37e89be3a193929963684adf36df35d2abe0
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2022-04-15 12:10:01 -0300

    Comments at MainActivity.kt

--- app/src/main/java/com/cursokotlin/quiz/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/quiz/MainActivity.kt
@@ -7,17 +7,31 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
+// Declares the class MainActivity as one of AppCompatActivity type
 class MainActivity : AppCompatActivity() {
+
+    // The function onCreate does things right on the instancing of an object.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // The visual elements are defined in activity_main.xml
         setContentView(R.layout.activity_main)
 
+        // Connects the Button btnStart to the visual element in activity_main.xml
         val btnStart : Button = findViewById(R.id.btn_start)
+
+        // Connects the Edit Text etName to the visual element in activity_main.xml
         val etName : EditText = findViewById(R.id.et_name)
+
+        // The setOnClickListener sets the btnStart
         btnStart.setOnClickListener {
+
+            // If the field is empty it shows a Toast asking to fill it.
             if (etName.text.isEmpty()) {
                 Toast.makeText(this, "Please enter your name.", Toast.LENGTH_LONG).show()
             }
+
+            // Otherwise it starts the app.
             else {
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
 
